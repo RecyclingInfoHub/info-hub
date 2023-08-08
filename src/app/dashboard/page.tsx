06a45d09da173eca7f09a4859aae7eb0082e8fcc
Darky Tan <wuichin.tan@tngdigital.com.my>
2023-08-09 00:59:58 +0800

    feat: partial integration on dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,16 +3,33 @@
 
 import { useLocations } from '@/firebase/locations';
 import { Container, Typography } from '@mui/material';
+import { ILocation, IMaterials } from '@/types';
+import dynamic from 'next/dynamic';
+import { useMaterials } from '@/firebase/materials';
+import MapFilter from '@/components/MapFilter';
+import { useSelectMaterial } from '@/hooks/select-material';
+
+const DynamicMap = dynamic(() => import('@/components/Map'), { ssr: false });
 
 export default function Dashboard() {
-  const [locations] = useLocations();
+  const [materials] = useMaterials() as unknown as [IMaterials[] | undefined];
+  const [materialFilterList, toggleMaterialFilterHandler] = useSelectMaterial();
+  const [locations] = useLocations(materialFilterList) as unknown as [
+    ILocation[] | undefined
+  ];
 
   return (
     <Container>
       <Typography variant="h1">Dashboard</Typography>
-      {locations?.map((location) => (
+      {/* {locations?.map((location) => (
         <Typography key={location.id}>{location.name}</Typography>
-      ))}
+      ))} */}
+      <MapFilter
+        rawMaterials={materials}
+        materialFilterList={materialFilterList}
+        toggleMaterialFilterHandler={toggleMaterialFilterHandler}
+      />
+      <DynamicMap locations={locations} />
     </Container>
   );
 }

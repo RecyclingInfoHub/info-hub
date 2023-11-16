48f43a040394fb6160a7994640c42d3dad9583a4
Kelok Chan <kelokchan@gmail.com>
2023-11-16 22:15:29 +0800

    Add current position getter and marker

--- src/utils/leaflet.ts
+++ src/utils/leaflet.ts
@@ -20,3 +20,12 @@ L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x.src,
   shadowUrl: markerShadow.src,
 });
+
+const LeafIcon = L.Icon.extend({
+  options: {},
+});
+
+export const greenIcon = new LeafIcon({
+  iconUrl:
+    'https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|2ecc71&chf=a,s,ee00FFFF',
+});
